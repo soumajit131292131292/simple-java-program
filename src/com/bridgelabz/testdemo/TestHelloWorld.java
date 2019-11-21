5a5adf6ff3994118fa2516970a6095c5641c0eb4
soumajit131292 <soumajit131292@gmail.com>
2019-11-21 12:38:03 +0530

    2nd commit

---
+++ src/com/bridgelabz/testdemo/TestHelloWorld.java
@@ -0,0 +1,12 @@
+package com.bridgelabz.testdemo;
+
+public class TestHelloWorld {
+public static void main(String[] args)
+{
+		int i=0;
+		while(i<10) {
+		System.out.println("hello world");
+		i++;
+	}
+}
+}
